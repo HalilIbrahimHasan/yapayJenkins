012a8ba42ac3ec998b02aa16d8194a69a6023b07
Ibrahim Kalin <hisetkal@gmail.com>
2025-03-19 03:06:39 -0400

    second

--- src/test/java/org/example/steps/SampleSteps.java
+++ src/test/java/org/example/steps/SampleSteps.java
@@ -11,22 +11,16 @@ public class SampleSteps {
     
     @Given("I am on the homepage")
     public void iAmOnTheHomepage() {
-        DriverManager.getDriver().get("https://www.google.com");
+        DriverManager.getDriver().get("https://chatgpt.com/");
     }
 
     @When("I perform some action")
     public void iPerformSomeAction() {
-        String title = DriverManager.getDriver().getTitle();
-        System.out.println("Page title: " + title);
-        Assert.assertTrue("Page title should contain 'Google'", 
-            title.contains("Google"));
+
     }
 
     @Then("I should see the expected result")
     public void iShouldSeeTheExpectedResult() {
-        String currentUrl = DriverManager.getDriver().getCurrentUrl();
-        System.out.println("Current URL: " + currentUrl);
-        Assert.assertTrue("URL should contain 'google'", 
-            currentUrl.contains("google"));
+
     }
 } 
