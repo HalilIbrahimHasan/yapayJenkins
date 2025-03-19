61c7373f2ed7e98997f3d2261a1a592449d71583
Ibrahim Kalin <hisetkal@gmail.com>
2025-03-19 05:16:29 -0400

    Add ChatGPT step definitions and page object

--- src/test/java/org/example/pages/GPTPage.java
+++ src/test/java/org/example/pages/GPTPage.java
@@ -1,7 +1,29 @@
 package org.example.pages;
 
-public class GPTPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
-
-
+public class GPTPage extends BasePage {
+    
+    @FindBy(css = "textarea[placeholder='Send a message']")
+    private WebElement messageInput;
+    
+    @FindBy(css = "button[type='submit']")
+    private WebElement submitButton;
+    
+    @FindBy(css = ".markdown-content")
+    private WebElement responseContent;
+    
+    public void enterQuestion(String question) {
+        messageInput.clear();
+        messageInput.sendKeys(question);
+    }
+    
+    public void submitQuestion() {
+        submitButton.click();
+    }
+    
+    public String getResponse() {
+        return responseContent.getText();
+    }
 }
